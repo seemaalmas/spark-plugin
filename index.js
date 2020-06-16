4e56377cd6cf0a29be808941a120ed358e47d381
seemaalmas <seemaalmasshaikh@gmail.com>
2020-06-17 01:19:48 +0530

    Sorting of column best bid

--- index.js
+++ index.js
@@ -84,37 +84,35 @@ function connectCallback() {
 *Function written for sorting of table columns
 */
 function sortTable(){
-  var table, i, x, y; 
-  table = document.getElementById("table"); 
-  var switching = true; 
+    var table, i, x, y
+  table = document.getElementById('table')
+  var switching = true
 
-  // Run loop until no switching is needed 
-  while (switching) { 
-      switching = false; 
-      var rows = table.rows; 
+  // Run loop until no switching is needed
+  while (switching) {
+    switching = false
+    var rows = table.rows
 
-      // Loop to go through all rows 
-      for (i = 1; i < (rows.length - 1); i++) { 
-          var Switch = false; 
+    // Loop to go through all rows
+    for (i = 1; i < rows.length - 1; i++) {
+      var Switch = false
 
-          // Fetch 2 elements that need to be compared 
-          x = rows[i].getElementsByTagName("TD")[6]; 
-          y = rows[i + 1].getElementsByTagName("TD")[6]; 
+      // Fetch 2 elements that need to be compared
+      x = rows[i].getElementsByTagName('TD')[1]
+      y = rows[i + 1].getElementsByTagName('TD')[1]
 
-          // Check if 2 rows need to be switched 
-          if (Number(x.innerHTML) > Number(y.innerHTML)) 
-              { 
-
-              // If yes, mark Switch as needed and break loop 
-              Switch = true; 
-              break; 
-          } 
-      } 
-      if (Switch) { 
-          // Function to switch rows and mark switch as completed 
-          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]); 
-          switching = true; 
-      } 
+      // Check if 2 rows need to be switched
+      if (Number(x.innerHTML) > Number(y.innerHTML)) {
+        // If yes, mark Switch as needed and break loop
+        Switch = true
+        break
+      }
+    }
+    if (Switch) {
+      // Function to switch rows and mark switch as completed
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i])
+      switching = true
+    }
   }
 }
 
